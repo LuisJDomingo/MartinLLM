b0d9851cebeae8dcf2e53d58d6bd225d17297716
LuisJDomingo <luisdomingogarcia79@gmail,com>
2025-08-13 12:54:47 +0200

    CI: Configurar despliegue automático a GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MartinLLM/', // 👈 nombre EXACTO del repo en GitHub
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    host: true,
-    fs: {
-      // Allow serving files from one level up to the project root
-      allow: [path.resolve(__dirname, '..')],
-    },
-  },
+  base: '/MartinLLM/',
 })
