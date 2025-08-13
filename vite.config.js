031678a53479a2990f12dbf9ca1c7a0a65ff85a4
LuisJDomingo <luisdomingogarcia79@gmail,com>
2025-08-13 14:40:45 +0200

    feat: Configure Vite for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MartinLLM/',
+  // Asegúrate de reemplazar 'martin-llm-web' con el nombre exacto de tu repositorio en GitHub.
+  base: '/martinLLM/',
 })
