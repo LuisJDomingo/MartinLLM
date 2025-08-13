a5393247c27861e97528b8ed152d886679b30ee7
LuisJDomingo <luisdomingogarcia79@gmail,com>
2025-08-13 13:13:25 +0200

    FIX: Add react-router-dom and configure basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
+    <BrowserRouter basename="/MartinLLM/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
